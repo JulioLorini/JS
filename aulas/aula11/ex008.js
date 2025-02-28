a12ec6a0e43fdabc8ec9e02d056ce3cd065357c6
M&L Sistemas <juliocesar.lorini04@gmail.com>
2025-02-28 13:02:00 -0300

    .

---
+++ aulas/aula11/ex008.js
@@ -0,0 +1,5 @@
+var vel = 50
+if (vel > 60){ //condição simples
+    console.log(`Voce foi multado pois ultrapassou a velocidade permitida, transitando a ${vel} Km/h`)
+}
+console.log('Dirija sempre com cinto de segurança')
