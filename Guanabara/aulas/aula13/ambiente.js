9cf1d8dfd27d9030deb2b83de18c54a78c9de8c0
M&L Sistemas <juliocesar.lorini04@gmail.com>
2025-03-05 14:28:52 -0300

    .

---
+++ Guanabara/aulas/aula13/ambiente.js
@@ -0,0 +1,26 @@
+
+var c = 1
+while(c <= 5){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+//Ambas dão o mesmo resultado
+
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while(c <= 5)
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
